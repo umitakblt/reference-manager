d31c153070d9334653ad5122671c48f497ef00dc
deneme <com.deneme>
2025-07-09 10:17:40 +0300

    Initial commit

---
+++ src/main/java/com/umitakbulut/reference_manager/ReferenceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.umitakbulut.reference_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferenceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReferenceManagerApplication.class, args);
+	}
+
+}
